e49aabc9ef5497a7da51ec3553c143c951935933
Ivan Tadeu Ferreira Antunes Filho <itadeufa@gmail.com>
2016-04-29 22:19:33 -0400

    Adds windows code

--- Input/InputClientLib.py
+++ Input/InputClientLib.py
@@ -1,5 +1,4 @@
 import time
-import curses
 import Transportation.Sockets.ClientSocketUDP as Client
 import Transportation.Sockets.ServerSocketUDP as Server
 
@@ -12,30 +11,61 @@ DEBUG = 0
 
 def inputProcess(port, host='localhost', player=1):
     clientSocket = [Client.ClientSocketUDP(host,port)]
-    def sendKey(stdscr):
-        while True:
-            c = stdscr.getkey()
-            if c == "KEY_F(5)":
+    try:
+        import curses
+        def sendKey(stdscr):
+            while True:
+                c = stdscr.getkey()
+                if c == "KEY_F(5)":
+                    try:
+                        serverSocket = Server.ServerSocketUDP('',port)
+                        clientSocket2 = Client.ClientSocketUDP(Config.host,Config.port)
+                        clientSocket2.sendData("")
+                        serverSocket.sock.settimeout(1)
+                        newIP= serverSocket.getData()
+                        clientSocket[0] = Client.ClientSocketUDP(newIP,port)
+                        print "New IP is " + newIP
+                        serverSocket.close()
+                    except:
+                        print "Could not get new ip"
+
+                        
                 try:
-                    serverSocket = Server.ServerSocketUDP('',port)
-                    clientSocket2 = Client.ClientSocketUDP(Config.host,Config.port)
-                    clientSocket2.sendData("")
-                    serverSocket.sock.settimeout(1)
-                    newIP= serverSocket.getData()
-                    clientSocket[0] = Client.ClientSocketUDP(newIP,port)
-                    print "New IP is " + newIP
-                    serverSocket.close()
+                    clientSocket[0].sendData(str(player)+'#'+c)
                 except:
-                    print "Could not get new ip"
+                    pass
+                if DEBUG:
+                    print c
+        curses.wrapper(sendKey)
 
-                    
-            try:
-                clientSocket[0].sendData(str(player)+'#'+c)
-            except:
-                pass
-            if DEBUG:
-                print c
-    curses.wrapper(sendKey)
+    except: #Using windows derp
+        from msvcrt import getch
+        def sendKey():
+            while True:
+                c = msvcrt.getch()
+                C= ord(c)
+                if C == 224: #function key
+                    c = msvcrt.getch()
+                    if c == '?':
+                        try:
+                            serverSocket = Server.ServerSocketUDP('',port)
+                            clientSocket2 = Client.ClientSocketUDP(Config.host,Config.port)
+                            clientSocket2.sendData("")
+                            serverSocket.sock.settimeout(1)
+                            newIP= serverSocket.getData()
+                            clientSocket[0] = Client.ClientSocketUDP(newIP,port)
+                            print "New IP is " + newIP
+                            serverSocket.close()
+                        except:
+                            print "Could not get new ip"
 
+                else:    
+                    try:
+                        clientSocket[0].sendData(str(player)+'#'+c)
+                    except:
+                        pass
+                    if DEBUG:
+                       print c
+        sendKey()
 
 
